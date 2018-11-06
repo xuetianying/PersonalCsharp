dcb8e3725a9509997e0d2248690030a3fcd9c59e
xuetianying <775085964@qq.com>
2018-11-06 11:43:07 +0800

    新增测试和类库

---
+++ test/PersonalLib.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using CommomLib;
+using Xunit;
+
+namespace PersonalLib.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Class1.Say();
+        }
+    }
+}
